af3fba515e816412561c30e7baffa71e3ae84f86
nolaurence <nolaurence@outlook.com>
2021-11-23 20:06:17 +0800

    first commit

---
+++ src/test/java/cn/nolaurence/anno/AnnoApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.nolaurence.anno;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnnoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
